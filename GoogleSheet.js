0c3083480174c6a4850d0b0f3f4a27f41f56dd1b
Shifaghare <138295553+Shifaghare@users.noreply.github.com>
2024-09-29 16:43:45 +0530

    update

--- GoogleSheet.js
+++ GoogleSheet.js
@@ -7,3 +7,20 @@ form.addEventListener('submit',e=>{
     .then(()=>{window.location.reload(); })
     .catch(error => console.error ('Error!',error.message))
 })
+const leftBtn = document.querySelector('.left-btn');
+const rightBtn = document.querySelector('.right-btn');
+const container = document.querySelector('.customers-container');
+
+leftBtn.addEventListener('click', () => {
+  container.scrollBy({
+    left: -300, // Adjust the scroll distance
+    behavior: 'smooth'
+  });
+});
+
+rightBtn.addEventListener('click', () => {
+  container.scrollBy({
+    left: 300, // Adjust the scroll distance
+    behavior: 'smooth'
+  });
+});
