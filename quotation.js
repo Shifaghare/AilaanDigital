9a1c2041a169302d929c8ff4d6af224ff8c9aaf8
Shifaghare <138295553+Shifaghare@users.noreply.github.com>
2024-10-05 17:45:23 +0530

    .

--- quotation.js
+++ quotation.js
@@ -1,6 +1,6 @@
 
 const scriptURL = 'https://script.google.com/macros/s/AKfycbygvEzj51pf3EWx9yRdZ0n8CP-rzY3E5PldQrnog_zkttETJo9YQdvcTA70lsbMNA/exec';
-const form = document.forms['quotation-form'];
+const form = document.forms['contact-form'];
 
 form.addEventListener('submit', e => {
     e.preventDefault(); // Prevent default form submission behavior
@@ -13,13 +13,14 @@ form.addEventListener('submit', e => {
     .then(() => {
         alert("Your form has been submitted!");
         setTimeout(() => {
-            window.location.href = "/index.html"; // Redirect to homepage
-        }, 500);
+            window.location.href = "/Homepage.html"; // Redirect to homepage
+        }, 100);
     })
     .catch(error => {
         console.error('Error! Failed to fetch', error);
     });
 });
+
 // Get popup and button elements
 const popup = document.getElementById("quotePopup");
 const btn = document.getElementById("getQuoteBtn");
